8daeae531512505bf108ae1a7d41932e7bf8c7fe
haseebshaik00 <haseebshaik00@gmail.com>
2025-04-14 21:38:45 -0700

    Refactor code

--- HW1-Rahman-ShaikHaseebUr/T1/script.py
+++ HW1-Rahman-ShaikHaseebUr/T1/script.py
@@ -3,9 +3,14 @@ import pandas as pd
 import os
 
 def fetch_one_stock(ticker_symbol, period='1mo'):
-    df = yf.download(ticker_symbol, period=period)
+    df = yf.download(ticker_symbol, period=period, group_by='ticker')
+    if isinstance(df.columns, pd.MultiIndex):
+        df.columns = ['_'.join(col).strip() if isinstance(col, tuple) else col for col in df.columns]
     df.reset_index(inplace=True)
-    df = df[['Date', 'Open', 'High', 'Low', 'Close', 'Volume']]
+    cols = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
+    df = df[[col for col in df.columns if any(c in col for c in cols)]]
+    df.columns = cols
+
     return df
 
 def fetch_stocks(ticker_list, period='1mo'):
@@ -18,11 +23,12 @@ def save_stocks_to_csv(stock_data_dict, folder='stockdata'):
 
 def test_submission():
     tickers = ['XOM', 'CVX', 'HAL', 'MMM', 'CAT', 'DAL', 'MCD', 'NKE', 'KO',
-               'JNJ', 'PFE', 'UNH', 'JPM', 'GS', 'BAC', 'APPL', 'MSFT',
+               'JNJ', 'PFE', 'UNH', 'JPM', 'GS', 'BAC', 'AAPL', 'MSFT',
                'NVDA', 'GOOGL', 'META']
     period = '2y'
     stock_data = fetch_stocks(tickers, period)
     save_stocks_to_csv(stock_data)
+    # generate_boxplot()
 
 if __name__ == '__main__':
     test_submission()
